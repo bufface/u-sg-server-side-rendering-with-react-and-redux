44584b7c6a9117ed534bb5f65e297347aa3919ae
Cristian Buffa <cristian.buffa@valtech.com>
2018-01-25 11:11:32 -0300

    Server Side Webpack

--- server/src/index.js
+++ server/src/index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.send(content);
 });
 
-app.listen(300, () => {
+app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
--- server/webpack.server.js
+++ server/webpack.server.js
@@ -15,7 +15,23 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
 
   // Tell webpack to run babel on every file it runs through
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            'react',
+            'stage-0',
+            ['env', { targets: { browser: ['last 2 versions'] } }]
+          ]
+        }
+      }
+    ]
+  }
 };
