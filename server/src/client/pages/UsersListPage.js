cd4a4c9b06d3c20e7255c299636458273e9d5b26
Cristian Buffa <cristian.buffa@valtech.com>
2018-02-11 20:37:34 -0300

    Refactoring to Pages

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUsers } from '../actions';
+import { fetchUsers } from '../actions/index';
 
 class UsersList extends Component {
   componentDidMount() {
