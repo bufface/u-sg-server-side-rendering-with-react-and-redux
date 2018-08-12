75379640faf67ce3710e038292b3f089035aecc0
Cristian Buffa <bufface@gmail.com>
2018-08-12 17:19:52 -0300

    React Helmet in Practice

--- server/src/client/pages/UsersListPage.js
+++ server/src/client/pages/UsersListPage.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
+import { Helmet } from 'react-helmet';
 
 class UsersList extends Component {
   componentDidMount() {
     this.props.fetchUsers();
   }
-  
+
   renderUsers() {
     return this.props.users.map(user => {
-      return <li key={ user.id }>{ user.name }</li>;
+      return <li key={user.id}>{user.name}</li>;
     });
   }
-  
+
   render() {
     return (
       <div>
+        <Helmet>
+          <title>Users App</title>
+          <meta property="og:title" content="Users App" />
+        </Helmet>
         Here's a big list of users:
-        <ul>
-          { this.renderUsers() }
-        </ul>
+        <ul>{this.renderUsers()}</ul>
       </div>
     );
   }
@@ -37,5 +40,8 @@ export { loadData };
 
 export default {
   loadData,
-  component: connect(mapStateToProps, { fetchUsers })(UsersList)
+  component: connect(
+    mapStateToProps,
+    { fetchUsers }
+  )(UsersList)
 };
